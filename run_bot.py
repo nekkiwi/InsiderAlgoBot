739aa6e854380d1173cbaed2bf41448413b93eab
Nekki97 <nektarios.winter@yahoo.de>
2025-07-14 23:04:42 +0200

    test action

--- run_bot.py
+++ run_bot.py
@@ -25,7 +25,7 @@ def main(args):
     ####################
 
     print("\n--- Scraping and Preprocessing New Data ---")
-    current_features_df = feature_scraper.run(num_business_days=20)
+    current_features_df = feature_scraper.run(num_business_days=100)
     if current_features_df is None or current_features_df.empty:
         print("No new data scraped. Exiting.")
         return
